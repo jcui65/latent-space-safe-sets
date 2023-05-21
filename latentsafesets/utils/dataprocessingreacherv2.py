7cf66c697b379871957aa7e764e12b0c987ca0cb
jcui65 <cuijn@seas.upenn.edu>
2023-05-21 18:55:19 +0000

    8th commit in n2!

--- latentsafesets/utils/dataprocessingreacherv2.py
+++ latentsafesets/utils/dataprocessingreacherv2.py
@@ -16,7 +16,8 @@ import latentsafesets.utils.plot_utils as pu
 @click.option('--fh', default=500, help='five hundred or 250 or 1000', type=int)#1000, 250#
 @click.option('--env',default='reacher',help='the environment',type=str)#reacher or push#
 def main(date, time,fh,env):
-    outputdir='/home/cuijin/Project6remote/latent-space-safe-sets/outputs/2023-'
+    #outputdir='/home/cuijin/Project6remote/latent-space-safe-sets/outputs/2023-'
+    outputdir='/home/jcui65/latent-space-safe-sets/outputs/2023-'
     #mar24='03-24'#mar23='03-23'#mar22='03-22'#mar25='03-25'#mar26='03-26'#mar27='03-27'#mar28='03-28'
     #date=mar28#mar22#mar24#mar26#mar27#mar25#mar23#
     #time='23-17-32'#'23-16-17'#'11-51-10'#'19-45-47'#'19-45-12'#'13-24-32'#'14-02-35'#'01-09-11'#'01-07-55'#'01-06-51'#'01-03-46'#'20-29-18'#'20-28-35'#'20-26-13'#'20-23-19'#'00-08-25'#'00-04-54'#'00-02-15'#
